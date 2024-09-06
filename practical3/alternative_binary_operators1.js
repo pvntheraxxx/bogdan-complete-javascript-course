7c3439a5c2e7cf751c5a0a4d7a9f5ea91548844b
pvntheraxxx <pvntheraxxx@gmail.com>
2024-09-06 18:03:33 +0300

    new little changes

---
+++ practical3/alternative_binary_operators1.js
@@ -0,0 +1,18 @@
+/* Альтернативные бинарные операторы */
+
+
+let a = 5;
+
+a *= 2
+console.log(a); // 10
+
+a -= 5;
+console.log(a); // 5
+
+a /= 2;
+console.log(a); // 2.5
+
+a *= 2;
+console.log(a); // 5
+
+
